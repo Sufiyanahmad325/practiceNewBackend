5426ab09866ecef9a51296e3835520d16dafc98d
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2024-10-21 07:32:51 +0530

    logout function has been completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -107,3 +107,26 @@ export const getCurrentUser = asyncHandler(async(req ,res)=>{
      )
  
  })
+
+
+
+
+
+
+ export const logOutUser = asyncHandler(async(req , res)=>{
+
+
+  
+
+        const options ={
+            httpOnly:true,
+            secure:true
+        }
+
+        return res.status(200)
+        .clearCookie("accessToken" , options)
+        .json(
+            new ApiResponse(200, {} , "user logged out")
+        )
+
+ })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import { getCurrentUser, loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -8,7 +8,7 @@ const router = Router()
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/current-user").get(verifyToken,getCurrentUser) 
-
+router.route("/logout").get(verifyToken,logOutUser) 
 
 
 
